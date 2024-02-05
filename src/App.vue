d87e03a1c57524b98e13e722550c91854c04f2d3
Artur Vasconcelos <artur.vasconcelos8@gmail.com>
2024-02-05 14:00:45 -0400

    exemplos de utilizacao VUE

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,80 @@
-<script setup></script>
+<script setup>
+import { reactive } from "vue";
+
+const nome = "artur";
+const meuObjeto = {
+  nome: "artur",
+  filmeFavorito: "Interstelar",
+};
+
+function dizOla(nome) {
+  return `${nome} diz oi`;
+}
+
+const imagemDoBatman =
+  "https://images.bauerhosting.com/legacy/empire-tmdb/films/272/images/65JWXDCAfwHhJKnDwRnEgVB411X.jpg?ar=16%3A9&fit=crop&crop=top&auto=format&w=1440&q=80";
+
+const imagemTomasMoreEoRei =
+  "https://rainhastragicas.files.wordpress.com/2012/10/a-man-for-all-seasons.jpg?w=640";
+
+const botaoEstaDesabilitado = false;
+
+const mostrarImagemBatman = false;
+const mostrarImagemTomasMores = false;
+
+const estaAutorizado = false;
+
+// let contador = 0;
+const estado = reactive({
+  contador: 0,
+  email: "",
+});
+
+function incrementar() {
+  estado.contador++;
+}
+
+function decrementar() {
+  estado.contador--;
+}
+
+function alteraEmail(evento) {
+  estado.email = evento.target.value;
+}
+</script>
 
 <template>
-  <h1>Olá</h1>
+  <h1>{{ dizOla("Artur") }}</h1>
+  <img v-if="mostrarImagemBatman" :src="imagemDoBatman" alt="" />
+  <img v-else-if="mostrarImagemTomasMores" :src="imagemTomasMoreEoRei" alt="" />
+  <h2 v-else>Nao quer ver imagens</h2>
+
+  <h1 v-if="estaAutorizado">Bem-vindo</h1>
+  <h1 v-else>Não possui acesso</h1>
+
+  <button :disabled="botaoEstaDesabilitado">enviar mensagem</button>
+
+  <br />
+  <hr />
+
+  {{ estado.contador }}
+
+  <button @click="incrementar" type="button">+</button>
+  <button @click="decrementar" type="button">-</button>
+
+  <br />
+  <hr />
+
+  {{ estado.email }}
+  <input type="email" @keyup="alteraEmail" />
+  <!-- 
+    @keyup = insere o conteudo instantaneamente 
+    @change = insere o conteudo somente apos sair do campo
+  -->
 </template>
 
-<style scoped></style>
+<style scoped>
+img {
+  max-width: 200px;
+}
+</style>
